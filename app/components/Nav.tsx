391ea0c3d8962797dd562c3f462e0e3fda7b9116
Ike <ikelimjl@gmail.com>
2023-07-09 23:59:47 +0800

    removed bold nav feature

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,14 +4,6 @@ import { APP_NAME } from "../globals";
 import Button from "./Button";
 
 export default function Nav() {
-  const [currentUrl, setCurrentUrl] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (typeof window != "undefined") {
-      setCurrentUrl(window.location.pathname);
-    }
-  }, []);
-
   return (
     <div className="flex w-full justify-center items-center  sticky top-0 left-0 z-20 bg-[#f9f2ffa2] shadow-md backdrop-blur-md">
       <div className="flex items-center justify-between px-4 py-2 max-w-[1800px] w-full">
@@ -25,21 +17,21 @@ export default function Nav() {
         <div className="flex items-center justify-end gap-8">
           {/* Utility Buttons */}
           <ul className="flex gap-7 justify-center items-center text-violet-700 nav-items font-semibold">
-            <li className={currentUrl === "/features" ? "font-bold" : ""}>
+            <li>
               <a href="/features">Features</a>
             </li>
-            <li className={currentUrl === "/docs" ? "font-bold" : ""}>
+            <li>
               <a href="/docs">Docs</a>
             </li>
-            <li className={currentUrl === "/inspirations" ? "font-bold" : ""}>
+            <li>
               <a href="/inspirations">Inspirations</a>
             </li>
-            <li className={currentUrl === "/community" ? "font-bold" : ""}>
+            <li>
               <a href="/community">Community</a>
             </li>
           </ul>
 
-          <ul className="flex gap-2 justify-center items-center">
+          <ul>
             <li>
               <Button
                 text="Sign Up"
