a17a3c0a31a83aeba2d29b81f7f6aa20ba1d55c4
Ike <ikelimjl@gmail.com>
2023-07-11 10:05:58 +0800

    changed background colour

--- app/components/FeatureBanner.tsx
+++ app/components/FeatureBanner.tsx
@@ -1,69 +1,56 @@
 export default function FeatureBanner() {
+  let feautured_lst = [
+    {
+      name: "Theme Generator",
+      link: "theme_generator",
+      alt: "Theme",
+      icon: "icon_theme.svg",
+    },
+    {
+      name: "Gradients",
+      link: "gradients",
+      alt: "Gradients",
+      icon: "icon_gradient.svg",
+    },
+    {
+      name: "Font Pairs",
+      link: "font_pairing",
+      alt: "Font Pairs",
+      icon: "icon_fonts.svg",
+    },
+    {
+      name: "Icons",
+      link: "icons",
+      alt: "Icons",
+      icon: "icon_icons.svg",
+    },
+  ];
   return (
     <div>
-      <div className="z-20 w-full grid place-items-center feature-banner">
-        <div className="w-full bg-violet-100 rounded-md p-8 shadow-lg ring-1 ring-violet-50/10 text-center">
+      <div className="z-20 w-full grid place-items-center">
+        <div className="w-full feature-banner rounded-md p-8 shadow-2xl ring-1 ring-violet-200/10 text-center">
           <h1 className="text-3xl text-indigo-950 font-bold">
-            Ignite Your Creative Spark with{" "}
-            <span className="gradient">ArtGen</span>
+            Ignite Your Creative Spark
           </h1>
-          <h4 className="m-3 text-lg font-bold text-orange-600">
-            Our Popular Picks
-          </h4>
-          <ul className="flex flex-wrap gap-4 justify-around items-center w-full text-indigo-950 mb-5">
-            <li className="ring-1 ring-slate-900/5 shadow-md">
-              <a href="/features/theme_generator">
-                <div className="flex flex-col gap-3 items-center justify-center">
-                  <img
-                    src="/features/icon_theme.svg"
-                    alt="Theme"
-                    className="h-14"
-                  />
-                  <p>Theme Generator</p>
-                </div>
-              </a>
-            </li>
-            <li className="ring-1 ring-slate-900/5 shadow-md">
-              <a href="/features/gradients">
-                <div className="flex flex-col gap-3 items-center justify-center">
-                  <img
-                    src="/features/icon_gradient.svg"
-                    alt="Gradients"
-                    className="h-14"
-                  />
-                  <p>Gradients</p>
-                </div>
-              </a>
-            </li>
-            <li className="ring-1 ring-slate-900/5 shadow-md">
-              <a href="/features/font_pairing">
-                <div className="flex flex-col gap-3 items-center justify-center">
-                  <img
-                    src="/features/icon_fonts.svg"
-                    alt="Fonts"
-                    className="h-14"
-                  />
-                  <p>Font Pairs</p>
-                </div>
-              </a>
-            </li>
-            <li className="ring-1 ring-slate-900/5 shadow-md">
-              <a href="/features/icons">
-                <div className="flex flex-col gap-3 items-center justify-center">
-                  <img
-                    src="/features/icon_icons.svg"
-                    alt="Icons"
-                    className="h-14"
-                  />
-                  <p>Icons</p>
-                </div>
-              </a>
-            </li>
+          <p className="m-3">
+            Popular design and digital art tools available with us.
+          </p>
+          <ul className="my-10 flex flex-wrap gap-10 justify-around items-center w-full text-indigo-950">
+            {feautured_lst.map((item) => (
+              <li>
+                <a href={`/features/${item.link}`}>
+                  <div className="flex flex-col gap-3 items-center justify-center">
+                    <img src={`/features/${item.icon}`} alt={item.alt} />
+                    <p className="font-semibold text-lg">{item.name}</p>
+                  </div>
+                </a>
+              </li>
+            ))}
           </ul>
 
           <div className="grid place-items-center">
             <a href="/features">
-              <p className="hover:translate-x-5 duration-300 ease-in-out text-violet-700 font-semibold">
+              <p className="hover:translate-x-5 duration-300 ease-in-out text-violet-800 font-semibold">
                 View all features
               </p>
             </a>
