ecfc8ec5cc8df56b276f5fdaa908ae84f7f0d22a
Ike <ikelimjl@gmail.com>
2023-07-12 20:22:25 +0800

    fixed responsiveness of frameworks banner

--- components/Frameworks.tsx
+++ components/Frameworks.tsx
@@ -4,7 +4,7 @@ export function FrameworkList() {
     <ul className="flex items-center gap-5 justify-around my-10 flex-wrap">
       {frameworkList.map((item) => (
         <li className="flex items-center gap-2 justify-center hover:scale-95 duration-100 ease-in-out flex-col">
-          <span className="bg-white rounded-full p-3">
+          <span className="bg-violet-50 rounded-full p-3 shadow-lg ">
             <img className="h-10" src={`./frameworks/${item}.svg`} alt={item} />
           </span>
           <p className="font-semibold cursor-default">{item}</p>
@@ -22,7 +22,9 @@ interface Props {
 export function FrameworkHeading({ header, subheader }: Props) {
   return (
     <div>
-      <h2 className="text-3xl font-bold text-violet-950 mb-3">{header}</h2>
+      <h2 className="min-[300px]:text-3xl text-xl font-bold text-violet-950 mb-3">
+        {header}
+      </h2>
       <p>{subheader}</p>
     </div>
   );
