20243cda25564cfb1fecf99162e77ba45a31b88b
Ike <ikelimjl@gmail.com>
2023-07-17 20:17:41 +0800

    dashboard layout

---
+++ app/account/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+import { SideBar } from "@/components/account/Dashboard";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex">
+      <SideBar />
+      <div className=" w-full sm:w-4/5 flex-5">{children}</div>
+    </div>
+  );
+}
