4a48fb9c575c7361a592b3156d07b59ea9f22a11
Ike <ikelimjl@gmail.com>
2023-07-18 10:24:46 +0800

    added default URL

--- app/globals.tsx
+++ app/globals.tsx
@@ -1,4 +1,5 @@
 export const APP_NAME = "ArtGen";
+export const DEFAULT_URL = "http://localhost:3000";
 
 export const FOOTER_HEADERS = ["Overview", "Company", "Resources"];
 export const APP_LINKS = [
