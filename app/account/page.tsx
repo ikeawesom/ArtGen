53354d554b8bb4d84fb5c2b93dd3235bacd52ce4
Ike <ikelimjl@gmail.com>
2023-07-21 11:18:42 +0800

    supabase - added auth session handler

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -32,46 +32,48 @@ export default function Page() {
     setLogIn(!logIn);
   };
 
-  if (!sess) {
-    return (
-      <div className="min-h-screen overflow-hidden">
-        <a href="/community">
-          <div className="heading sm:hover:top-8 hover:bg-slate-50 duration-200 bg-white flex gap-2 items-center justify-center select-none w-fit px-5 py-2 shadow-lg rounded-md absolute sm:top-5 sm:left-1/2 sm:-translate-x-1/2 z-20">
-            <img src="/favicon.svg" alt="ArtGen" width={50} />
-            <div>
-              <h1 className="text-3xl m-0 p-0">
-                <span className="gradient">ArtGen</span>
-              </h1>
-              <p className="m-0 p-0 text-sm">Community</p>
+  return (
+    <div>
+      {!sess && (
+        <div className="min-h-screen overflow-hidden">
+          <a href="/community">
+            <div className="heading sm:hover:top-8 hover:bg-slate-50 duration-200 bg-white flex gap-2 items-center justify-center select-none w-fit px-5 py-2 shadow-lg rounded-md absolute sm:top-5 sm:left-1/2 sm:-translate-x-1/2 z-20">
+              <img src="/favicon.svg" alt="ArtGen" width={50} />
+              <div>
+                <h1 className="text-3xl m-0 p-0">
+                  <span className="gradient">ArtGen</span>
+                </h1>
+                <p className="m-0 p-0 text-sm">Community</p>
+              </div>
             </div>
+          </a>
+          <div className="flex h-full main-container">
+            <FormContainer className="bg-gradient-to-b from-violet-200 to-cyan-200 relative sub-container">
+              <Circle color="bg-green-200 z-0" position="bottom-10 left-10" />
+              <Circle color="bg-pink-300 z-0" position="top-10 right-10" />
+              <Display />
+            </FormContainer>
+            <FormContainer className="sub-container">
+              {!logIn && (
+                <AccountForm
+                  title="Create an account"
+                  user={false}
+                  onClick={handleSwitchForm}
+                />
+              )}
+              {logIn && (
+                <AccountForm
+                  title="Log in"
+                  user={true}
+                  onClick={handleSwitchForm}
+                />
+              )}
+            </FormContainer>
           </div>
-        </a>
-
-        <div className="flex h-full main-container">
-          <FormContainer className="bg-gradient-to-b from-violet-200 to-cyan-200 relative sub-container">
-            <Circle color="bg-green-200 z-0" position="bottom-10 left-10" />
-            <Circle color="bg-pink-300 z-0" position="top-10 right-10" />
-            <Display />
-          </FormContainer>
-          <FormContainer className="sub-container">
-            {!logIn && (
-              <AccountForm
-                title="Create an account"
-                user={false}
-                onClick={handleSwitchForm}
-              />
-            )}
-            {logIn && (
-              <AccountForm
-                title="Log in"
-                user={true}
-                onClick={handleSwitchForm}
-              />
-            )}
-          </FormContainer>
         </div>
-      </div>
-    );
-  }
-  return <LoadingScreen text="Redirecting to dashboard..." />;
+      )}
+
+      {sess && <LoadingScreen text="Redirecting to dashboard..." />}
+    </div>
+  );
 }
