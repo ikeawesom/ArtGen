98d834c3b7dbded3efb8b3811a9ed3b0aec0621e
Ike <ikelimjl@gmail.com>
2023-07-21 09:47:06 +0800

    added features metadata

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,5 +1,10 @@
 import Footer from "@/components/Footer";
 import Nav from "@/components/utilities/Nav";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Features",
+};
 
 export default function RootLayout({
   children,
