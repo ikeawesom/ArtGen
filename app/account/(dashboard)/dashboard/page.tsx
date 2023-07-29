4ac1e33ebff52d1c16b5592ee1869e4ac7bf0c38
Ike <ikelimjl@gmail.com>
2023-07-29 23:08:37 +0800

    added notice for completing of account

--- app/account/(dashboard)/dashboard/page.tsx
+++ app/account/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,21 @@
+"use client";
 import CompleteAccount from "@/components/account/CompleteAccount";
+import { useState } from "react";
 
 export default function Page() {
+  const [displayCompleteAccModal, setModalDisplay] = useState(true);
+
+  function handleModal() {
+    setModalDisplay(!displayCompleteAccModal);
+  }
+
   return (
     <div>
       <h1 className="font-bold text-3xl">Dashboard</h1>
       <hr className="mb-6" />
-      <CompleteAccount />
+      {displayCompleteAccModal && (
+        <CompleteAccount closeAction={() => handleModal()} />
+      )}
       <h1>Features</h1>
     </div>
   );
--- components/account/CompleteAccount.tsx
+++ components/account/CompleteAccount.tsx
@@ -1,10 +1,13 @@
-"use client";
 import { useEffect, useState } from "react";
 import { getUserDetails } from "@/supabase/auth/handleAuth";
 import profilesDB from "@/supabase/database/handleProfiles";
 import "./account.css";
 
-export default function CompleteAccount() {
+interface Props {
+  closeAction: () => void;
+}
+
+export default function CompleteAccount({ closeAction }: Props) {
   const [incompleteTasks, setIncompleteTasks] = useState<any[]>();
   const totalTasks = 4;
   const [percent, setPercent] = useState(0);
@@ -15,14 +18,14 @@ export default function CompleteAccount() {
     if (userInformation.display_name === null)
       temp.push({
         a: "/account",
-        icon: "icon_avatar.svg",
+        icon: "icon_name.svg",
         name: "Display Name",
       });
 
     if (userInformation.profile_picture === null)
       temp.push({
         a: "/account/settings",
-        icon: "icon_name.svg",
+        icon: "icon_avatar.svg",
         name: "Profile Picture",
       });
 
@@ -56,24 +59,34 @@ export default function CompleteAccount() {
 
   useEffect(() => {
     if (incompleteTasks) {
-      const percent_temp = (1 - incompleteTasks.length / totalTasks) * 100 + 50;
+      const percent_temp = (1 - incompleteTasks.length / totalTasks) * 100;
       setPercent(percent_temp);
     }
   }, [incompleteTasks]);
 
   if (incompleteTasks)
     return (
-      <div>
+      <div className="border-2 border-violet-400 rounded-md p-8 pb-5 my-5 relative bg-violet-200 notice">
         <div className="flex gap-2 items-center justify-start">
-          <h1 className="font-bold text-indigo-900">
-            Finish setting up your account
+          <h1 className="font-bold text-indigo-900 text-xl">
+            Spice up your account!
           </h1>
-          <h4 className="text-violet-600 text-sm">{percent}%</h4>
+          <h4 className="text-violet-600 text-md">{percent}%</h4>
         </div>
 
-        <div className="h-5 w-full rounded-lg bg-slate-50 my-3 shadow-md overflow-hidden">
+        <div className="h-5 w-full rounded-2xl bg-slate-50 my-3 shadow-md overflow-hidden">
           <div
-            className={`h-5 progress-bar bg-violet-400 w-[${percent}%]`}
+            className={`h-full progress-bar bg-violet-400 ${
+              percent === 0
+                ? "w-0"
+                : percent === 25
+                ? "w-1/4"
+                : percent === 50
+                ? "w-1/2"
+                : percent === 75
+                ? "w-3/4"
+                : "w-full"
+            }`}
           ></div>
         </div>
         <ul className="flex gap-3 overflow-x-auto pr-3 py-3">
@@ -96,6 +109,13 @@ export default function CompleteAccount() {
             </a>
           ))}
         </ul>
+        <img
+          onClick={closeAction}
+          src="/icons/icon_cross.svg"
+          alt=""
+          width={20}
+          className="cursor-pointer hover:brightness-150 duration-150 absolute top-6 right-6"
+        />
       </div>
     );
 }
