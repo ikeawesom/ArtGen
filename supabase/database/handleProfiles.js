3abf3d3afe84bf4bf559871987c54a67d16d1520
Ike <ikelimjl@gmail.com>
2023-08-13 04:18:24 +0800

    polymorphed main class

--- supabase/database/handleProfiles.js
+++ supabase/database/handleProfiles.js
@@ -1,5 +1,22 @@
 import mainDBClass from "./handleDB";
+import supabase from "../config";
 
-const profilesDB = new mainDBClass("profiles");
+class mainProfilesDB extends mainDBClass {
+  constructor(table_name) {
+    super();
+    this.table_name = table_name;
+  }
+
+  async setUsername(value, id) {
+    const { error } = await supabase
+      .from(this.table_name)
+      .update({ display_name: value })
+      .eq("id", id);
+
+    if (error) return error;
+    return "success";
+  }
+}
+const profilesDB = new mainProfilesDB("profiles");
 
 export default profilesDB;
